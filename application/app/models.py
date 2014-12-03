bae644dba135a2de275213b3edec5d6d7d0b9bdc
Dennis Sell <sell.dennis@gmail.com>
2014-12-02 22:49:48 -0500

    Added models

--- application/app/models.py
+++ application/app/models.py
@@ -1,5 +1,56 @@
 from application import db
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    rapGodPoints = db.Column(db.Integer, default=0)
+
+    def __init__(self, username):
+        self.username = username
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+
+class Rap(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True)
+    completed = db.Column(db.Boolean, default=False)
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return '<Rap %r>' % (self.title)
+
+
+class Line(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    line1 = db.Column(db.String(1000))
+    line2 = db.Column(db.String(1000))
+    isPending = db.Column(db.Boolean, default=True)
+
+    rapID = db.Column(db.Integer)
+    lineIndex = db.Column(db.Integer)
+
+    userID = db.Column(db.Integer)
+    owner = db.Column(db.String(80))
+
+    upvotes = db.Column(db.Integer, default=0)
+    downvotes = db.Column(db.Integer, default=0)
+
+    def __init__(self, line1, line2, lineIndex, rapID, userID, owner):
+        self.line1 = line1
+        self.line2 = line2
+        self.lineIndex = lineIndex
+        self.rapID = rapID
+        self.userID = userID
+        self.owner = owner
+
+    def __repr__(self):
+        return '<Lines %r\n\t%r>' % (self.line1, self.line2)
+
+
 class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(1000))
